4d34a461ff5e814d4d1905926fc016761b9e4117
Chloe Peng <chloepeng331@icloud.com>
2022-01-31 20:56:36 -0500

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 1;
+const num2 = 62;
+const multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random()*10 + 1);
+
+const num3 = 40;
+const num4 = 10;
+const mod = num3/num4;
+
+const max = Math.max(0, 5, 20, 4, 5);
